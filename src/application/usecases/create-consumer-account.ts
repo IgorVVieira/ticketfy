fede0b59a858dc93523bb3820728093e79b6d8ce
IgorVVieira <igor.gutoo63@gmail.com>
2023-07-28 15:39:47 -0300

    feat: CreateConsumerAccount use case

---
+++ src/application/usecases/create-consumer-account.ts
@@ -0,0 +1,25 @@
+import { ConsumerAccount } from "../../domain/entities/consumer-accounts";
+import { IConsumerAccountRepository } from "../../domain/repositories/consumer-account.repository";
+import { PaymentEnum } from "../../domain/entities/payment-enum";
+
+type CreateConsumerAccountProps = {
+  consumer_id: string;
+  name: string;
+  amount: number;
+  type: string;
+};
+
+export class CreateConsumerAccount {
+  constructor(
+    private readonly consumerAccountRepo: IConsumerAccountRepository
+  ) {}
+
+  async execute(input: CreateConsumerAccountProps): Promise<ConsumerAccount> {
+    const consumerAccount = ConsumerAccount.create({
+      ...input,
+      type: PaymentEnum[input.type as keyof typeof PaymentEnum],
+    });
+
+    return this.consumerAccountRepo.create(consumerAccount);
+  }
+}
