8c496659c0068e49bdcafda523761fd3679cec72
IgorVVieira <igor.gutoo63@gmail.com>
2023-07-28 16:23:53 -0300

    feat: CreateConsumer use case

---
+++ src/application/usecases/consumer/create-consumer.ts
@@ -0,0 +1,18 @@
+import { Consumer } from "../../../domain/entities/consumer/consumer";
+import { UserProps } from "../../../domain/entities/user";
+import { IUserRepository } from "../../../domain/repositories/user.repository";
+
+export class CreateConsumer {
+  constructor(private readonly consumerRepository: IUserRepository) {}
+
+  async execute(input: UserProps) {
+    let consumer = await this.consumerRepository.findByEmail(input.email);
+
+    if (consumer) {
+      throw new Error("Email already in use");
+    }
+
+    consumer = Consumer.create(input);
+    return this.consumerRepository.create(consumer);
+  }
+}
