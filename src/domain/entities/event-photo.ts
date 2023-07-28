1d3437af9f9cd4a61d128785d1e298c4e8f3cbac
IgorVVieira <igor.gutoo63@gmail.com>
2023-07-28 14:57:51 -0300

    feat: EventPhoto entity

---
+++ src/domain/entities/event-photo.ts
@@ -0,0 +1,16 @@
+import { Entity } from "../../core/domain/Entity";
+
+type EventPhotoProps = {
+  event_id: string;
+  url: string;
+};
+
+export class EventPhoto extends Entity<EventPhotoProps> {
+  private constructor(props: EventPhotoProps, id?: string) {
+    super(props, id);
+  }
+
+  static create(props: EventPhotoProps, id?: string): EventPhoto {
+    return new EventPhoto(props, id);
+  }
+}
