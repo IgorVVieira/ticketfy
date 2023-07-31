35c6b9689fc62374b49d57d56f107fd75aac12f1
IgorVVieira <igor.gutoo63@gmail.com>
2023-07-31 16:35:28 -0300

    feat: create events table

---
+++ src/infra/database/migrations/1690831759219-CreateEventPhotosTable.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateEventPhotosTable1690831759219 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+    await queryRunner.createTable(
+      new Table({
+        name: "event_photos",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "event_id",
+            type: "uuid",
+          },
+          {
+            name: "url",
+            type: "varchar",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "event_photos_events",
+            referencedTableName: "events",
+            referencedColumnNames: ["id"],
+            columnNames: ["event_id"],
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("event_photos");
+    await queryRunner.query(`DROP EXTENSION "uuid-ossp"`);
+  }
+}
