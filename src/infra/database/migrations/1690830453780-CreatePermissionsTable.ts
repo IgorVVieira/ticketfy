99d99b5be7cc40570d4e2499281a8ef469dc7993
IgorVVieira <igor.gutoo63@gmail.com>
2023-07-31 16:09:32 -0300

    feat: CreatePermissionsTable migration

---
+++ src/infra/database/migrations/1690830453780-CreatePermissionsTable.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreatePermissionsTable1690830453780 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+    await queryRunner.createTable(
+      new Table({
+        name: "permissions",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+            isNullable: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("permissions");
+    await queryRunner.query(`DROP EXTENSION "uuid-ossp"`);
+  }
+}
