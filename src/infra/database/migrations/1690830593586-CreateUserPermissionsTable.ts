9474742f0444440d3b3595647d1b5f462cd89a38
IgorVVieira <igor.gutoo63@gmail.com>
2023-07-31 16:13:32 -0300

    feat: CreateUserPermissionsTable migration

--- src/infra/database/migrations/1690830593586-CreateUserPermissionsTable.ts
+++ src/infra/database/migrations/1690830593586-CreateUserPermissionsTable.ts
@@ -1,11 +1,13 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreatePermissionsTable1690830453780 implements MigrationInterface {
+export class CreateUserPermissionsTable1690830593586
+  implements MigrationInterface
+{
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
     await queryRunner.createTable(
       new Table({
-        name: "permissions",
+        name: "user_permissions",
         columns: [
           {
             name: "id",
@@ -14,13 +16,12 @@ export class CreatePermissionsTable1690830453780 implements MigrationInterface {
             default: "uuid_generate_v4()",
           },
           {
-            name: "name",
-            type: "varchar",
+            name: "user_id",
+            type: "uuid",
           },
           {
-            name: "description",
-            type: "varchar",
-            isNullable: true,
+            name: "permission_id",
+            type: "uuid",
           },
           {
             name: "created_at",
@@ -33,12 +34,26 @@ export class CreatePermissionsTable1690830453780 implements MigrationInterface {
             default: "now()",
           },
         ],
+        foreignKeys: [
+          {
+            name: "user_permissions_user",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+          },
+          {
+            name: "user_permissions_permission",
+            referencedTableName: "permissions",
+            referencedColumnNames: ["id"],
+            columnNames: ["permission_id"],
+          },
+        ],
       })
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("permissions");
+    await queryRunner.dropTable("user_permissions");
     await queryRunner.query(`DROP EXTENSION "uuid-ossp"`);
   }
 }
