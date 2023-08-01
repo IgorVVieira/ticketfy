6c23bb5dd37156f02f4cb4a7d22ae2825baf2181
IgorVVieira <igor.gutoo63@gmail.com>
2023-08-01 17:14:10 -0300

    fix: remove logout

--- src/infra/controllers/auth.controller.ts
+++ src/infra/controllers/auth.controller.ts
@@ -4,7 +4,6 @@ import { Login } from "../../application/usecases/login";
 export class AuthController {
   constructor(private readonly loginUseCase: Login) {
     this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
   }
 
   async login(req: Request, res: Response) {
@@ -20,19 +19,6 @@ export class AuthController {
       if (error.message === "User not found") {
         return res.status(404).json({ message: error.message });
       }
-      return res.status(500).json({ message: error.message });
-    }
-  }
-
-  async logout(req: Request, res: Response) {
-    try {
-      const token = req.headers.authorization?.split(" ")[1];
-      if (!token) {
-        return res.status(401).json({ message: "Token not found" });
-      }
-      // this.jwtService.blacklistToken(token);
-      return res.json({ message: "Logout successfully" });
-    } catch (error: any) {
       return res.status(500).json({ message: error.message });
     }
   }
