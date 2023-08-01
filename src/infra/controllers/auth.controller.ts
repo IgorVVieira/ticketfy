c26c7deefa72e9a9fb8c8ddc1bcac17b002bfbce
IgorVVieira <igor.gutoo63@gmail.com>
2023-07-31 23:06:42 -0300

    docs: improve swagger doc

--- src/infra/controllers/auth.controller.ts
+++ src/infra/controllers/auth.controller.ts
@@ -14,10 +14,10 @@ export class AuthController {
 
       return res.json({ token });
     } catch (error: any) {
-      if (
-        error.mesaage === "User not found" ||
-        error.message === "Password does not match"
-      ) {
+      if (error.message === "Password does not match") {
+        return res.status(409).json({ message: error.message });
+      }
+      if (error.message === "User not found") {
         return res.status(404).json({ message: error.message });
       }
       return res.status(500).json({ message: error.message });
